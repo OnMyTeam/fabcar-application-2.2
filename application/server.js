f8f5fd4e496ae9ba49704fe6f7b60bbee10af39a
OnMyTeam <sangiki07@gmail.com>
2020-09-06 16:07:53 +0900

    modify ccpPath

--- application/server.js
+++ application/server.js
@@ -12,7 +12,7 @@ const { Gateway, Wallets } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
-const ccpPath = path.resolve(__dirname, '..', 'network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+const ccpPath = path.resolve(__dirname, '..', '..', 'network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
 const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
 
 const walletPath = path.join(process.cwd(), 'wallet');
